e76ec99ce7639abc8de285d52efb80b9e265d69d
T01320 <diego.rangel@compasso.com.br>
2023-07-12 17:54:33 -0300

    Aula 29 - Removendo projetos

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -12,6 +12,7 @@ import styles from './Projects.module.css'
 function Projects(){
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState('false')
+    const [projectMessage, setProjectMessege] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -32,8 +33,22 @@ function Projects(){
                 setRemoveLoading(true)
               })
               .catch((err) => console.log(err))
-        },)
+        }, 300)
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then((resp) => resp.json())
+          .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessege('Projeto removido com sucesso!')
+          })
+          .catch((err) => console.log(err))
+    }
 
     return(
         <div className={styles.project_container}>
@@ -42,6 +57,7 @@ function Projects(){
                 <LinkButton to='/newproject' text='Criar Projeto'/>
             </div>
             {message && <Message type="success" msg={message} /> }
+            {projectMessage && <Message type="success" msg={projectMessage} /> }
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) =>
@@ -51,11 +67,12 @@ function Projects(){
                         budget={project.budget}
                         category={project.category.name}
                         key={project.id}
+                        handleRemove={removeProject}
                         />
                     )
                 }
-                {!setRemoveLoading && <Loading /> }
-                {setRemoveLoading && projects.length === 0 && (
+                {!removeLoading && <Loading /> }
+                {removeLoading && projects.length === 0 && (
                     <p>Não há projetos cadastrado!</p>
                 )}
             </Container>
