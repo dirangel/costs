7a783548b9a56a9486b47147c43f58c620b6e04b
T01320 <diego.rangel@compasso.com.br>
2023-07-18 17:17:34 -0300

    Aula 34 - Adicionando novos serviços

--- src/components/Pages/Project.js
+++ src/components/Pages/Project.js
@@ -1,3 +1,5 @@
+import { parse, v4 as uuidv4 } from 'uuid'
+
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
@@ -5,6 +7,7 @@ import Loading from '../layout/Loading'
 import Container from '../layout/Container'
 import ProjectForm from  '../project/ProjectForm'
 import Message from '../layout/Message'
+import ServiceForm from '../service/ServiceForm'
 
 
 function Project(){
@@ -32,6 +35,41 @@ function Project(){
           }, 500);
         }, [id])
 
+        function createService(project){
+          setMessage('')
+
+          const lastService = project.services[project.services.length -1]
+
+          lastService.id = uuidv4()
+
+          const lastServiceCost = lastService.cost
+
+          const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+          //maximum value validation
+          if(newCost > parseFloat(project.budget)) {
+            setMessage('Orçamento ultrapassado, verifique o valor do serviço.')
+            setType('error')
+            project.services.pop()
+            return false
+          }
+
+          //add service cost to project total cost
+          project.cost = newCost
+
+          // update project
+          fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+              'content-type': 'application/json'
+            },
+            body: JSON.stringify(project)
+          }).then(resp => resp.json())
+            .then((data) => {
+              console.log(data)
+            })
+            .catch((err) => console.log(err))
+        }
 
 
         function editPost(project){
@@ -59,7 +97,6 @@ function Project(){
             setType('success')
           })
           .catch((err) => console.log(err))
-
         }
 
         function toggleProjectForm(){
@@ -104,20 +141,24 @@ function Project(){
                       )}
                     </div>
                     <div className={styles.service_form_container}>
-                        <h2>Adicione um serviço:</h2>
-                        <button className={styles.btn} onClick={toggleServiceForm}>
+                      <h2>Adicione um serviço:</h2>
+                      <button className={styles.btn} onClick={toggleServiceForm}>
                         {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
                       </button>
                       <div className={styles.project_info}>
                         {showServiceForm &&
-                          <div>Formulário do serviço</div>
+                          <ServiceForm
+                            handleSubmit={createService}
+                            btnText="Adicionar serviço"
+                            projectData={project}
+                          />
                         }
                       </div>
-                      <h2>Serviços</h2>
-                      <Container>
-                        <p>Itens de serviços</p>
-                      </Container>
                     </div>
+                    <h2>Serviços</h2>
+                    <Container>
+                      <p>Itens de serviços</p>
+                    </Container>
                </div>
               ) : (<Loading/>)}
            </>
