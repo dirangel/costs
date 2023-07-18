ce74a29c619774fc0d80cc972d676976f7536814
T01320 <diego.rangel@compasso.com.br>
2023-07-17 23:22:22 -0300

    Aula 32 - Edição de mensagem de sucesso

--- src/components/Pages/Project.js
+++ src/components/Pages/Project.js
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import ProjectForm from  '../project/ProjectForm'
+import Message from '../layout/Message'
+
 
 function Project(){
 
@@ -10,6 +13,8 @@ function Project(){
 
         const [ project, setProject ] = useState([])
         const [showProjectForm, setShowProjectForm] = useState(false)
+        const [message, setMessage] = useState()
+        const [type, setType] = useState()
 
         useEffect(() => {
           setTimeout(() => {
@@ -26,15 +31,45 @@ function Project(){
           }, 1000);
         }, [id])
 
+
+
+        function editPost(project){
+          console.log('Budget: ', project.budget)
+          console.log('Cost: ', project.cost)
+          if(project.budget < project.cost){
+            setMessage('O orçamento não pode ser menor que o custo do projeto')
+            setType('error')
+            return false
+          }
+
+          const fet = fetch(`http://localhost:5000/projects/${project.id}`, {
+          method: 'PATCH',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify(project),
+        }).then(resp => resp.json())
+          .then((data) => {
+            setProject(data)
+            setShowProjectForm(false)
+            setMessage('Projeto atualizado!')
+            setType('success')
+          })
+          .catch((err) => console.log(err))
+
+        }
+
         function toggleProjectForm(){
           setShowProjectForm(!showProjectForm)
         }
+
 
         return(
             <>
              {project.name ? (
                <div className={styles.project_details}>
-                  <Container customClass="column"></Container>
+                  <Container customClass="column">
+                    {message && <Message  type={type} msg={message}/>}
                     <div className={styles.details_container}>
                       <h1>Projeto: {project.name}</h1>
                       <button className={styles.btn} onClick={toggleProjectForm}>
@@ -49,15 +84,20 @@ function Project(){
                             <span>Total de orçamento: </span> {project.budget}
                           </p>
                           <p>
-                            <span>Total Utilizado: </span> R${project.budget}
+                            <span>Total Utilizado: </span> R${project.cost}
                           </p>
                         </div>
                       ) : (
                         <div className={styles.project_info}>
-                          <p>Detalhe do projeto</p>
+                          <ProjectForm
+                            handleSubmit={editPost}
+                            btnText="Concluir edição"
+                            projectData={project}
+                          />
                         </div>
                       )}
                     </div>
+                    </Container>
                </div>
               ) : (<Loading/>)}
            </>
